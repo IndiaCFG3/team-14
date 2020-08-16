9cba0f6f881fa55df58bf4d9a519cbc1d21f1c9f
Shubham2503 <46487179+Shubham2503@users.noreply.github.com>
2020-08-16 11:44:39 +0530

    added initial requirements

---
+++ TBDF/django_twi/twilio_auth.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+
+def SID(request):
+    return HttpResponse("AC47d3b372588f0f8b935c43f7b18ca306")
+
+def TOKEN(request):
+    return HttpResponse("ffd2cbddbe938fcbcc64843f7fe60862")
