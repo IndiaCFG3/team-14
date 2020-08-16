1e86fcf2a1ac8f706066ae70a677d8896b783575
shayan yaseen <shayan.yaseen1@gmail.com>
2020-08-16 13:52:30 +0530

    twillio

---
+++ TBDF/tsms/secretkey.py
@@ -0,0 +1,9 @@
+# Django SECRET_KEY for sessions
+SECRET_KEY = 'development key'  # change this to a secret string when deploying
+
+# Twilio credentials and phone number
+# obtained from twilio.com/console
+TWILIO_ACCOUNT_SID = 'AC47d3b372588f0f8b935c43f7b18ca306 '
+# also obtained from twilio.com/console
+TWILIO_AUTH_TOKEN = 'ffd2cbddbe938fcbcc64843f7fe60862 '
+TWILIO_NUMBER = '+919714948808'  # use the number you received when signing up or buy a new number
--- TBDF/tsms/views.py
+++ TBDF/tsms/views.py
@@ -1,4 +1,4 @@
-from django.conf import settings
+from . import secretkey
 from django.http import HttpResponse
 from twilio.rest import Client
 
@@ -6,10 +6,10 @@ from twilio.rest import Client
 def tsms_sms(request):
     message_to_broadcast = ("Have you played the incredible TwilioQuest "
                             "yet? Grab it here: https://www.twilio.com/quest")
-    client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
-    for recipient in settings.SMS_BROADCAST_TO_NUMBERS:
+    client = Client(secretkey.TWILIO_ACCOUNT_SID, secretkey.TWILIO_AUTH_TOKEN)
+    for recipient in secretkey.SMS_BROADCAST_TO_NUMBERS:
         if recipient:
             client.messages.create(to=recipient,
-                                   from_=settings.TWILIO_NUMBER,
+                                   from_=secretkey.TWILIO_NUMBER,
                                    body=message_to_broadcast)
     return HttpResponse("messages sent!", 200)
