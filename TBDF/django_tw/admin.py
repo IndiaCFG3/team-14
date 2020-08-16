d3b47e7ccc156cb3d4504f9bea7392447a68bf6a
Shubham2503 <46487179+Shubham2503@users.noreply.github.com>
2020-08-16 12:23:14 +0530

    send_message
    add model to send message

--- TBDF/django_tw/admin.py
+++ TBDF/django_tw/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import message
 # Register your models here.
+
+admin.site.register(message)
--- TBDF/django_tw/models.py
+++ TBDF/django_tw/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from twilio.rest import Client
+from .auth import *
+
 
 # Create your models here.
+class message(models.Model):
+    result = models.PositiveIntegerField()
+    def __str__(self):
+        return str(self.result)
+
+    def save(self,*args,**kwargs):
+        if self.result < 70:
+            account_sid = sid
+            auth_token = token
+            client = Client(account_sid, auth_token)
+
+            message = client.messages.create(
+                                        body=f'Hi there! - {self.result}',
+                                        from_='+16039414130',
+                                        to='+919714948808'
+                                    )
+
+            print(message.sid)
+        return super().save(*args, **kwargs)
