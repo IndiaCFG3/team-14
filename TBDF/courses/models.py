9bd6df5f8fd2f9b650a1c0f4f0ebc5057b7ff014
Jasmeen <jasbansal56@gmail.com>
2020-08-16 12:19:29 +0530

    Added course model

--- TBDF/courses/models.py
+++ TBDF/courses/models.py
@@ -1,12 +1,19 @@
 from django.db import models
-
+from account.models import Teacher
 # Create your models here.
+
+#Course information
+class Course(models.Model):
+    teacher_id = models.ForeignKey(Teacher, verbose_name="Teacher ID")
+    course_id = models.IntegerField(verbose_name="Course ID")
+    name = models.TextField(verbose_name="Course Name")
+
 
 #Message Sending
 class MessageSender(models.Model):
     msg_id = models.IntegerField(verbose_name="Message ID")
     timestamp = models.DateTimeField(auto_now=True, verbose_name="Sent Time")
-    course_id = models.ForeignKey( ,verbose_name="Course ID")
+    course_id = models.ForeignKey(Course,verbose_name="Course ID")
     description = models.TextField(verbose_name="Description")
 
 #Message Receiving
