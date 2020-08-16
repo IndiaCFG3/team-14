16a842a0f9879a42660ad03cc653632ca3d36fdf
Praneeth021 <kadampallypraneeth987@gmail.com>
2020-08-16 11:05:27 +0530

    templates added

--- TBDF/TBDF/settings.py
+++ TBDF/TBDF/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'TBDF.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join('accounts/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
