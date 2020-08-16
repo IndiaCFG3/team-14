44938de86a8d8b0ac243a3dd6b9de49893e60b64
Shubham2503 <46487179+Shubham2503@users.noreply.github.com>
2020-08-16 14:16:33 +0530

    changed number

--- TBDF/tsms/secretkey.py
+++ TBDF/tsms/secretkey.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'development key'  # change this to a secret string when deploying
 TWILIO_ACCOUNT_SID = 'AC47d3b372588f0f8b935c43f7b18ca306 '
 # also obtained from twilio.com/console
 TWILIO_AUTH_TOKEN = 'ffd2cbddbe938fcbcc64843f7fe60862 '
-TWILIO_NUMBER = '+919714948808'  # use the number you received when signing up or buy a new number
+TWILIO_NUMBER = '+16039414130'  # use the number you received when signing up or buy a new number
 SMS_BROADCAST_TO_NUMBERS= [
      '+919714948808',
  ]
