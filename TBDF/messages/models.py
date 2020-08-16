44cbc031af045c92f61cd2caf7b8fdd96c61a252
Jasmeen <jasbansal56@gmail.com>
2020-08-16 12:39:01 +0530

    Added primary key

--- TBDF/messages/models.py
+++ TBDF/messages/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 #Message Sending
 class MessageSender(models.Model):
-    msg_id = models.IntegerField(verbose_name="Message ID")
+    msg_id = models.IntegerField(primary_key=True, verbose_name="Message ID")
     timestamp = models.DateTimeField(auto_now=True, verbose_name="Sent Time")
     course_id = models.ForeignKey(Course,verbose_name="Course ID")
     description = models.TextField(verbose_name="Description")
 
 #Message Receiving
 class MessageReceiver(models.Model):
-    msg_id = models.IntegerField(verbose_name="Message ID")
+    msg_id = models.IntegerField(primary_key=True, verbose_name="Message ID")
     timestamp = models.DateTimeField(auto_now=True, verbose_name="Received Time")
     mobile_number = models.ForeignKey(Student,verbose_name="Mobile Number")
     description = models.TextField(verbose_name="Description")
